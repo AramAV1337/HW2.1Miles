6d96c9039450253485c62350401b900f7e319c20
Aram Avetisov <aram.avetisov@inbox.ru>
2021-05-14 12:38:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int enominator = 20;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите стоимость билета рублях: ");
+        int amount = in.nextInt();
+        in.close();
+        int bonusMiles = amount / enominator; // Рассчет бонусных миль
+        System.out.println("Ваш бонус в милях равен: " +bonusMiles);
+    }
+}
